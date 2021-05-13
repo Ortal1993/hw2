957b76980c455ade219a03b2401d92a7201a9621
Ortal1993 <cohen1993@gmail.com>
2021-05-13 06:56:24 +0000

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/hw2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hw2.dir/hw2_test.cxx.o"
+  "CMakeFiles/hw2.dir/test1.cxx.o"
+  "libhw2.a"
+  "libhw2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hw2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ library.h
@@ -0,0 +1,6 @@
+#ifndef HW2_LIBRARY_H
+#define HW2_LIBRARY_H
+
+void hello();
+
+#endif //HW2_LIBRARY_H
